01ef810dbf352625be7c841f3ca247048ea01c17
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-28 16:52:14 +0900

    객체 메서드를 정의할 때 function 키워드를 사용하지 않아도 된다.

--- 2023.05.28/objectEnhancement.js
+++ 2023.05.28/objectEnhancement.js
@@ -9,3 +9,37 @@ const mountain = { name, elevation, print };
 console.log(mountain.print());
 
 // 객체 리터럴 개선을 통해 객체 메서드를 생산하는 것도 가능하다.
+
+// * 옛날 방식
+
+var skier = {
+  name: name,
+  sound: sound,
+  powderYell: function () {
+    var yell = this.sound.toUpperCase();
+    console.log(`${yell} ${yell} ${yell} !!!`);
+  },
+  speed: function (mph) {
+    this.speed = mph;
+    console.log("속력(mph):", mph);
+  },
+};
+
+console.log(skier);
+
+// * 새로운 방식
+
+const skieer = {
+  name,
+  sound,
+  powderYell() {
+    let yell = this.sound.toUpperCase();
+    console.log(`${yell} ${yell} ${yell} !!!`);
+  },
+  speed(mph) {
+    this.speed = mph;
+    console.log("속력(mph):", mph);
+  },
+};
+
+console.log(skieer);
