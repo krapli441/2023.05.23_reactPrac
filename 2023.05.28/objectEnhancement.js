65804c9d3f2c3e5b964582c37e3113cb54f11f2e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-28 16:43:11 +0900

    객체 리터럴 개선

---
+++ 2023.05.28/objectEnhancement.js
@@ -0,0 +1,8 @@
+const name = "탈락";
+const elevation = 9738;
+
+const mountain = { name, elevation };
+
+console.log(mountain); // name : "탈락", elevation : 9738
+
+// mountain 객체에는 name과 elevation이라는 필드가 들어가게 됐다.
