e0ba30e476168f0ca4f875b09a2a66f7ae95aa50
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-28 15:39:41 +0900

    템플릿 문자열

---
+++ 2023.05.28/template.js
@@ -0,0 +1,8 @@
+let firstName = "박";
+let middleName = "준형";
+let lastName = "krapli";
+
+console.log(lastName + "," + firstName + " " + middleName);
+
+console.log(`${lastName},${firstName} ${middleName}`);
+
