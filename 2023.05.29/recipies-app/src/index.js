5dccab55801dcf382c7d3f972299206adb096c46
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-29 16:29:39 +0900

    개발 의존 환경으로 webpack과 webpack-cli를 설치

---
+++ 2023.05.29/recipies-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import Menu from "../components/Menu.js";
+import data from "../src/data/recipes.json";
+
+// 위 네 문장은 앱이 작동하는데 필요한 모듈을 import한다.
