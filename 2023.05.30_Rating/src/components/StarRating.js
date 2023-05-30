0a51854f5216c76a3f9b6b68fd3cd7c1db34869e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-30 17:06:22 +0900

    useStateHook 이후 onClick 이벤트 추가

--- 2023.05.30_Rating/src/components/StarRating.js
+++ 2023.05.30_Rating/src/components/StarRating.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa/index.esm.js";
 
-const Star = ({ selected = false }) => {
-  return <FaStar color={selected ? "red" : "grey"} />;
+// const Star = ({ selected = false }) => {
+//   return <FaStar color={selected ? "red" : "grey"} />;
+// };
+// // Star 컴포넌트는 별 하나를 렌더링한다.
+// // 이 때, selected 프로퍼티에 따라 적절한 색으로 별 안쪽을 채워넣는다.
+// // selected 프로퍼티가 컴포넌트에 전달되지 않으면
+// // 별이 선택되지 않았다고 가정하고, 디폴트 색인 회색으로 칠한다.
+
+const Star = ({ selected = false, onsSelect = (f) => f }) => {
+  return <FaStar color={selected ? "red" : "grey"} onCilck={onSelect} />;
 };
-// Star 컴포넌트는 별 하나를 렌더링한다.
-// 이 때, selected 프로퍼티에 따라 적절한 색으로 별 안쪽을 채워넣는다.
-// selected 프로퍼티가 컴포넌트에 전달되지 않으면
-// 별이 선택되지 않았다고 가정하고, 디폴트 색인 회색으로 칠한다.
 
 const createArray = (length) => [...Array(length)];
 
-export default function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+export default function StarRating({ totalStars = 10 }) {
+  const [selectedStars] = useState(8);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => {
+        return <Star key={i} selected={selectedStars > i} />;
+      })}
+      <p>
+        {selectedStars} / {totalStars}
+      </p>
+    </>
+  );
 }
