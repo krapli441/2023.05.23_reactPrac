b1df0a2d6eb2178aa6e32199b5197caf12efb9bd
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-30 17:48:05 +0900

    별점 평가 컴포넌트

--- 2023.05.30_Rating/src/components/StarRating.js
+++ 2023.05.30_Rating/src/components/StarRating.js
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa/index.esm.js";
 
-// const Star = ({ selected = false }) => {
-//   return <FaStar color={selected ? "red" : "grey"} />;
-// };
-// // Star 컴포넌트는 별 하나를 렌더링한다.
-// // 이 때, selected 프로퍼티에 따라 적절한 색으로 별 안쪽을 채워넣는다.
-// // selected 프로퍼티가 컴포넌트에 전달되지 않으면
-// // 별이 선택되지 않았다고 가정하고, 디폴트 색인 회색으로 칠한다.
-
 const Star = ({ selected = false, onSelect = (f) => f }) => {
   return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
 };
