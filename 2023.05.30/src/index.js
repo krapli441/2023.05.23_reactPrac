0e81fcd0689c73381cf528537ae01738a75d26cb
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-30 11:33:54 +0900

    바벨 개발의존성 설치. preset 지정.

--- 2023.05.30/src/index.js
+++ 2023.05.30/src/index.js
@@ -2,3 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import Menu from "./components/Menu";
 import data from "./data/recipe.json";
+
+render(<Menu recipes={data} />), document.getElementById("root");
