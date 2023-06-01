15cade2463e7905346b225c6b64b45f5b22b24c5
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 15:55:47 +0900

    버튼도 적용된다.

--- 2023.06.01_router/src/components/Second.tsx
+++ 2023.06.01_router/src/components/Second.tsx
@@ -7,7 +7,11 @@ export default function Screen() {
       <div className="container">
         <h1>리액트 라우터를 실험해봅시다</h1>
         <h2>Second.tsx 두번째로 만든 페이지입니다</h2>
-        <Link to="/">다시 Screen으로 이동</Link>
+        <Link to="/">
+          <form className="button">
+            <button type="submit">버튼도 적용되나요?</button>
+          </form>
+        </Link>
       </div>
     </>
   );
