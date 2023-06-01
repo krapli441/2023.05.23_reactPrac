a7d52d86110228cc09a506eb3e7fd3ecf990846d
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 09:41:35 +0900

    ts로 사칙연산 애플리케이션

---
+++ 2023.06.01/src/components/devide.ts
@@ -0,0 +1,3 @@
+export default function devide(a: number, b: number) {
+  return a / b;
+}
---
+++ 2023.06.01/src/index.ts
@@ -0,0 +1,10 @@
+import add from "./components/add";
+import subtract from "./components/subtract";
+import mulitply from "./components/mulitply";
+import devide from "./components/devide";
+
+console.log(add(2, 5));
+console.log(subtract(4, 5));
+console.log(mulitply(2, 7));
+console.log(devide(10, 2));
+
