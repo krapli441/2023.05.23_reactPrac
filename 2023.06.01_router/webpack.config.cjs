a38b8792ab77fff983339e4fdf9b00aa5bf156b1
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 14:42:31 +0900

    라우터 테스트

---
+++ 2023.06.01_router/webpack.config.cjs
@@ -0,0 +1,30 @@
+let path = require("path");
+const { Extension } = require("typescript");
+
+module.exports = {
+  entry: {
+    index: "./src/index.ts",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  stats: {
+    errorDetails: true,
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        use: ["babel-loader"],
+      },
+      { test: [/\.ts$/, /\.tsx$/], use: ["babel-loader", "ts-loader"] },
+    ],
+  },
+  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+  devServer: {
+    historyApiFallback: true,
+    port: 8080,
+    hot: true,
+  },
+};
