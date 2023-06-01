af1f68584fb39195b1885100b8fd591cc9b760d1
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-01 15:12:06 +0900

    BrowserRouter 가져옴

--- 2023.06.01_router/src/components/Screen.tsx
+++ 2023.06.01_router/src/components/Screen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function App () {
+export default function Screen () {
   return (
     <>
     <div className="container">
-      <h1>히히 앱 컨테이너</h1>
+      <h1>히히 스크린 컨테이너</h1>
     </div>
     </>
   )
--- 2023.06.01_router/src/index.tsx
+++ 2023.06.01_router/src/index.tsx
@@ -1,6 +1,16 @@
+// ! 리액트 라이브러리
 import React from "react";
 import { createRoot, Root } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// ? 컴포넌트
 import App from "./components/App";
 
+
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+root.render(
+<BrowserRouter>
+  <App />
+</BrowserRouter>
+);
